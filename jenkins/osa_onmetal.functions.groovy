e0d188323711700d9d7aadb46c3c9e67901bb49a
lamarwhitej <lamarwhitej@yahoo.com>
2017-02-18 00:35:35 -0600

    Update osa_onmetal.functions.groovy

--- jenkins/osa_onmetal.functions.groovy
+++ jenkins/osa_onmetal.functions.groovy
@@ -487,7 +487,7 @@ def aggregate_parse_failed_smoke(host_ip, results_file, elasticsearch_ip) {
 def parse_results() {
 
     sh '''
-    elastic-upgrade -u $HOME/output/api.uptime.out -d $HOME/output/during.uptime.out -p $HOME/output/persistent_resource.txt -b $HOME/subunit/smoke/before_upgrade -a $HOME/subunit/smoke/after_upgrade
+    elastic-upgrade -u $HOME/output/api.uptime.out -f $HOME/output/swift_status.json -n $HOME/output/nova_status.json -k $HOME/output/keystone_status.json -b $HOME/subunit/smoke/before_upgrade -a $HOME/subunit/smoke/after_upgrade -p $HOME/output/persistent_resource.txt
     elastic-upgrade -s $HOME/output/nova_status.json,$HOME/output/swift_status.json,$HOME/output/keystone_status.json
     '''
 
