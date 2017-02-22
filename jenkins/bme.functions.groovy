f7b5c99790aeda99354d9caf1d91ad0afdb86c8b
Joshua White <joshua.l.white@intel.com>
2017-02-22 17:57:53 +0000

    Added Rally Scenarios to groovy script.  Install, run prime, and run benchmarks.

--- jenkins/bme.functions.groovy
+++ jenkins/bme.functions.groovy
@@ -238,6 +238,66 @@ def parse_persistent_resources_tests(controller_name='controller01', tempest_dir
     """
 }
 
+def install_rally(controller_name='controller01', tempest_dir=null) {
+    String host_ip = get_deploy_node_ip()
+    String container_ip = get_controller_utility_container_ip(controller_name)
+
+    echo 'Installing during upgrade test on ${controller}_utility container'
+    sh """
+        ssh -o StrictHostKeyChecking=no\
+        -o ProxyCommand='ssh -W %h:%p root@${host_ip}' root@${container_ip} '''
+            TEMPEST_DIR=${tempest_dir}
+            cd \$TEMPEST_DIR/
+            rm -rf rally/
+            wget -q -O- https://raw.githubusercontent.com/openstack/rally/master/install_rally.sh | bash
+            cd rally/
+            sudo git clone https://github.com/jwatford/rally-scenarios.git
+            cd rally-scenarios/
+            sudo python extract_values.py
+        '''
+    """
+}
+
+def prime_rally_benchmarks(controller_name='controller01', tempest_dir=null) {
+    String host_ip = get_deploy_node_ip()
+    String container_ip = get_controller_utility_container_ip(controller_name)
+
+    echo 'Installing during upgrade test on ${controller}_utility container'
+    sh """
+        ssh -o StrictHostKeyChecking=no\
+        -o ProxyCommand='ssh -W %h:%p root@${host_ip}' root@${container_ip} '''
+            cd /root/
+            . /root/rally/bin/activate
+            source openrc
+            rally-manage db recreate
+            rally deployment create --fromenv --name=existing
+            rally deployment use --deployment existing
+            cd \$TEMPEST_DIR/rally/rally-scenarios/
+            rally task start osic-keystone-prime-scenario.json --task-args-file args.yaml
+            rally task start osic-nova-1-server-scenario.json --task-args-file args.yaml
+        '''
+    """
+}
+
+def run_rally_benchmarks(controller_name='controller01', tempest_dir=null, results_file = 'results') {
+    String host_ip = get_deploy_node_ip()
+    String container_ip = get_controller_utility_container_ip(controller_name)
+
+    sh """
+        ssh -o StrictHostKeyChecking=no\
+        -o ProxyCommand='ssh -W %h:%p root@${host_ip}' root@${container_ip} '''
+            cd \$TEMPEST_DIR
+            . \$TEMPEST_DIR/rally/bin/activate
+            source openrc
+            rally deployment use --deployment existing
+            cd \$TEMPEST_DIR/rally/rally-scenarios/
+            rally task start benchmark.json --task-args-file args.yaml
+            rally task results > \$TEMPEST_DIR/output/${results_file}.json
+            deactivate
+        '''
+    """
+}
+
 def transfer_tempest_configuration_to_controller(controller_name='controller01'){
   String container_ip = get_controller_utility_container_ip(controller_name)
   String host_ip = get_deploy_node_ip()
