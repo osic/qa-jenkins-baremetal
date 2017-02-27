a703c436a4bae8cabd4b041eb090bd8105de74c7
jwatford <jonte.watford@intel.com>
2017-02-27 11:41:37 -0600

    Update bme.functions.groovy

--- jenkins/bme.functions.groovy
+++ jenkins/bme.functions.groovy
@@ -244,13 +244,18 @@ def copy_tempest_configuration_for_rally(controller_name='controller01'){
 
   try {
       sh """
-          scp -o StrictHostKeyChecking=no\
-          -o ProxyCommand='ssh -W %h:%p root@${container_ip}:/opt/tempest_untagged/etc/tempest.conf \
-          root@${host_ip}:/root/
 
-          scp -o StrictHostKeyChecking=no\
-          -o ProxyCommand='ssh -W %h:%p root@${container_ip}:/root/openrc \
-          root@${host_ip}:/root/
+          ssh -o StrictHostKeyChecking=no\
+          -o ProxyCommand='ssh -W %h:%p root@${host_ip}' root@${container_ip} '''
+
+             scp -o StrictHostKeyChecking=no\
+             -o ProxyCommand='ssh -W %h:%p root@${container_ip}:/opt/tempest_untagged/etc/tempest.conf \
+             root@${host_ip}:/root/
+
+             scp -o StrictHostKeyChecking=no\
+             -o ProxyCommand='ssh -W %h:%p root@${container_ip}:/root/openrc \
+             root@${host_ip}:/root/
+          '''
 
           scp -o StrictHostKeyChecking=no\
           -o ProxyCommand='ssh -W %h:%p root@${host_ip}:/root/openrc \
